170b0a1dacc62a585dfd14fdb49d946db0586798
Jonathan Jara <jonathanjara@Jonathans-MacBook-Pro.local>
2020-02-22 08:16:49 +0000

    fix project for test with travis

--- src/main/java/com/jjara/microservice/post/api/HandlerParameter.java
+++ src/main/java/com/jjara/microservice/post/api/HandlerParameter.java
@@ -6,13 +6,38 @@ package com.jjara.microservice.post.api;
  */
 public interface HandlerParameter<T> {
 
+    /**
+     * Gets the id parameter from the URL
+     * @param serverRequest
+     * @return
+     */
     Long id(T serverRequest);
 
+    /**
+     * Gets the page parameter from the URL
+     * @param serverRequest
+     * @return
+     */
     Integer page(T serverRequest);
 
+    /**
+     * Gets the size parameter from the URL
+     * @param serverRequest
+     * @return
+     */
     Integer size(T serverRequest);
 
+    /**
+     * Gets the tag parameter from the URL
+     * @param serverRequest
+     * @return
+     */
     Integer tag(T serverRequest);
 
+    /**
+     * Gets the title parameter from the URL
+     * @param serverRequest
+     * @return
+     */
     String title(T serverRequest);
 }
