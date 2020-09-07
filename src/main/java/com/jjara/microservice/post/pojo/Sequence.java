17cf11cc45997ae491ad24157dc9bdd2dc796ba3
JJaraM <jonathan.jara.morales@gmail.com>
2020-09-07 11:11:28 +0100

    Removed unused field

--- src/main/java/com/jjara/microservice/post/pojo/Sequence.java
+++ src/main/java/com/jjara/microservice/post/pojo/Sequence.java
@@ -5,17 +5,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "post_sequence")
 public class Sequence {
 
-	private String id;
 	private long seq;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
+	
 	public long getSeq() {
 		return seq;
 	}
