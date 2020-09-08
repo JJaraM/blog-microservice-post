301ff08a717605edba94fca48132592196f15fda
JJaraM <jonathan.jara.morales@gmail.com>
2020-09-08 11:08:13 +0100

    Add support for github packages

--- src/main/java/com/jjara/microservice/post/routers/IndexRouter.java
+++ src/main/java/com/jjara/microservice/post/routers/IndexRouter.java
@@ -1,12 +1,12 @@
 package com.jjara.microservice.post.routers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static com.jjara.ui.IndexPage.getIndex;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -15,7 +15,8 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 public class IndexRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> htmlRouter(@Value("classpath:/public/index.html") Resource html) {
-        return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(html));
+    public RouterFunction<ServerResponse> htmlRouter() {
+        String content = getIndex("Post Web Service","Post Web Service", "");
+        return route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(content));
     }
 }
