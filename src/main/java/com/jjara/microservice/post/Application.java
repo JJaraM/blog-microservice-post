e9b4360b2cd9c89ce1f7d247890182db1762f8c9
JJaraM <jonathan.jara.morales@gmail.com>
2020-09-10 11:32:06 +0100

    Add http trace

--- src/main/java/com/jjara/microservice/post/Application.java
+++ src/main/java/com/jjara/microservice/post/Application.java
@@ -1,7 +1,10 @@
 package com.jjara.microservice.post;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -18,5 +21,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
