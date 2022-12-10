b35a6a5f420232edc8a322cf510a2215def2ac2b
Jonathan Jara Morales <jonathan.jara.morales@gmail.com>
2022-12-10 20:34:22 +0000

    Fix imports issue

--- src/main/java/com/jjara/microservice/configuration/cors/CorsWebFilterConfiguration.java
+++ src/main/java/com/jjara/microservice/configuration/cors/CorsWebFilterConfiguration.java
@@ -1,5 +1,11 @@
 package com.jjara.microservice.configuration.cors;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 /**
  * Allows to configure the cors for the web service, for more details please visit:
  * https://www.baeldung.com/spring-webflux-cors
@@ -11,15 +17,15 @@ public class CorsWebFilterConfiguration {
 	@Value("${spring.cors.allowed-origin}") private List<String> allowedOrigins;
 
 	@Bean CorsWebFilter corsWebFilter() {
-		var config = new CorsConfiguration();
-		config.setAllowedOrigins(allowedOrigins);
-		config.setAllowedMethods(Arrays.asList(
-			HttpMethod.GET.name(),
-			HttpMethod.PUT.name(),
-			HttpMethod.POST.name(),
-			HttpMethod.DELETE.name()
-		));
-		config.addAllowedHeader("*");
+	    var config = new CorsConfiguration();
+	    config.setAllowedOrigins(allowedOrigins);
+	    config.setAllowedMethods(Arrays.asList(
+	        HttpMethod.GET.name(),
+		HttpMethod.PUT.name(),
+		HttpMethod.POST.name(),
+		HttpMethod.DELETE.name()
+	    ));
+	    config.addAllowedHeader("*");
 
 	    var source = new UrlBasedCorsConfigurationSource();
 	    source.registerCorsConfiguration("/**", config);
